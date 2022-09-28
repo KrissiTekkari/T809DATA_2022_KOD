d5ce736a5057d6153becebd0c6327631a8d2a7f1
KrissiTekkari <dadasonk@gmail.com>
2022-09-28 18:53:15 +0000

    allt búið í 08 nema independent

--- 08_SVM/template.py
+++ 08_SVM/template.py
@@ -1,6 +1,6 @@
-# Author: 
-# Date:
-# Project: 
+# Author: Kristjan Orri Dadason
+# Date: 28/09/2022
+# Project: 08_SVM
 # Acknowledgements: 
 #
 
@@ -9,6 +9,7 @@
 # Make sure to comment out or remove all unnecessary code before submitting.
 
 
+from unicodedata import name
 from tools import plot_svm_margin, load_cancer
 from sklearn import svm
 from sklearn.datasets import make_blobs
@@ -19,9 +20,10 @@ import matplotlib.pyplot as plt
 
 
 def _plot_linear_kernel():
-    X, t = make_blobs(...)
-    ...
-    plot_svm_margin(...)
+    X, t = make_blobs(n_samples=40, centers=2, random_state=2)
+    clf = svm.SVC(kernel='linear', C=1000)
+    clf.fit(X, t)
+    plot_svm_margin(clf, X, t)
 
 
 def _subplot_svm_margin(
@@ -45,27 +47,42 @@ def _subplot_svm_margin(
     # similar to tools.plot_svm_margin but added num_plots and
     # index where num_plots should be the total number of plots
     # and index is the index of the current plot being generated
-    ...
+    plt.subplot(1, num_plots, index)
+    plot_svm_margin(svc, X, t)
 
 
 def _compare_gamma():
-    X, t = make_blobs(n_samples=40, centers=2, random_state=6)
+    X, t = make_blobs(n_samples=40, centers=2, random_state=2)
 
-    clf = svm.SVC(...)
-    ...
+    clf = svm.SVC(kernel='rbf', C=1000)
+    clf.fit(X, t)
     _subplot_svm_margin(clf, X, t, 3, 1)
-
-    clf = ...
-    ...
+    plt.title('$\gamma$=default')
+    
+    clf = svm.SVC(kernel='rbf', C=1000, gamma = 0.2)
+    clf.fit(X, t)
     _subplot_svm_margin(clf, X, t, 3, 2)
+    plt.title('$\gamma=0.2$')
 
-    ...
+    clf = svm.SVC(kernel='rbf', C=1000, gamma = 2)
+    clf.fit(X, t)
+    _subplot_svm_margin(clf, X, t, 3, 3)
+    plt.title('$\gamma=2$')
+
 
     plt.show()
 
 
 def _compare_C():
-    ...
+    X, t = make_blobs(n_samples=40, centers=2, random_state=2)
+    
+    C_list = [1000, 0.5, 0.3, 0.05, 0.0001]
+    for i in range(len(C_list)):
+        clf = svm.SVC(kernel='linear', C=C_list[i])
+        clf.fit(X, t)
+        _subplot_svm_margin(clf, X, t, len(C_list), i+1)
+        plt.title('$C$={}'.format(C_list[i]))
+    plt.show()
 
 
 def train_test_SVM(
@@ -82,4 +99,34 @@ def train_test_SVM(
 
     This function should return (accuracy, precision, recall)
     '''
-    ...
+    svc.fit(X_train, t_train)
+    accuracy = accuracy_score(t_test, svc.predict(X_test))
+    precision = precision_score(t_test, svc.predict(X_test))
+    recall = recall_score(t_test, svc.predict(X_test))
+    return (accuracy, precision, recall)
+
+if __name__ == '__main__':
+    #_plot_linear_kernel()
+    #_compare_gamma()
+    #_compare_C()
+    np.random.seed(1234)
+    (X_train, t_train), (X_test, t_test) = load_cancer()
+    
+    svc_linear = svm.SVC(kernel='linear',C=1000)
+    linear_kernel = train_test_SVM(svc_linear, X_train, t_train, X_test, t_test)
+    
+    svc_sigmoid = svm.SVC(kernel='sigmoid',C=1000)
+    sigmoid_kernel = train_test_SVM(svc_sigmoid, X_train, t_train, X_test, t_test)
+    
+    svc_rbf = svm.SVC(kernel='rbf',C=1000)
+    rbf_kernel = train_test_SVM(svc_rbf, X_train, t_train, X_test, t_test)
+    
+    svc_poly = svm.SVC(kernel='poly',C=1000)
+    poly_kernel = train_test_SVM(svc_poly, X_train, t_train, X_test, t_test)
+    
+    print("                          accuracy           precision             recall")
+    print('Linear kernel:     ', linear_kernel)
+    print('Sigmoid kernel:    ', sigmoid_kernel)
+    print('RBF kernel:        ', rbf_kernel)
+    print('Polynomial kernel: ', poly_kernel)
+    
