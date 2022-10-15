9ea23bbce9df50fd0e4ed835cf27aa4616342f9a
KrissiTekkari <dadasonk@gmail.com>
2022-10-15 00:01:33 +0000

    Everything finished in 09, except the independent section, finish that later

--- 09_random_forests/template.py
+++ 09_random_forests/template.py
@@ -40,6 +40,7 @@ class CancerClassifier:
 
         # Fit the classifier to the training data here
         self.classifier.fit(self.X_train, self.t_train)
+        # Predict the labels of the test data here
         self.predictions = self.classifier.predict(self.X_test)
 
     def confusion_matrix(self) -> np.ndarray:
@@ -90,7 +91,24 @@ class CancerClassifier:
         for the current classifier and return a list of
         indices, sorted by feature importance (high to low).
         '''
-        ...
+        feature_importances = self.classifier.feature_importances_
+        # Sort from higher to lower importance
+        sorted_indices = np.argsort(feature_importances)[::-1]
+        # make list of strings of sorted_indices (for xticks)
+        sorted_indices_str = [str(i) for i in sorted_indices]
+        N = range(len(feature_importances))
+        # make the bar plot look nicer ;)
+        cmap = plt.get_cmap('coolwarm')
+        colors = cmap(feature_importances[sorted_indices])
+        plt.bar(N, feature_importances[sorted_indices], color = colors)
+        plt.xticks(N, sorted_indices_str, rotation=90)
+        plt.xlabel("Feature index")
+        plt.ylabel("Feature importance")
+        plt.tight_layout()
+        plt.gca().spines['right'].set_visible(False)
+        plt.gca().spines['top'].set_visible(False)
+        plt.show()
+        return sorted_indices
 
 
 def _plot_oob_error():
@@ -121,13 +139,14 @@ def _plot_oob_error():
     # Map a classifier name to a list of (<n_estimators>, <error rate>) pairs.
     error_rate = OrderedDict((label, []) for label, _ in ensemble_clfs)
 
-    min_estimators = 30
-    max_estimators = 175
-
+    min_estimators = 20
+    max_estimators = 250
+    
+    cancer = load_breast_cancer()
     for label, clf in ensemble_clfs:
         for i in range(min_estimators, max_estimators + 1):
             clf.set_params(n_estimators=i)
-            clf.fit(...,  ...)  # Use cancer data here
+            clf.fit(cancer.data, cancer.target)  # Use cancer data here
             oob_error = 1 - clf.oob_score_
             error_rate[label].append((i, oob_error))
 
@@ -142,10 +161,58 @@ def _plot_oob_error():
     plt.legend(loc="upper right")
     plt.show()
 
-
 def _plot_extreme_oob_error():
-    ...
+    RANDOM_STATE = 1337
+    ensemble_clfs = [
+        ("ExtraTreesClassifier, max_features='sqrt'",
+            ExtraTreesClassifier(
+                n_estimators=100,
+                bootstrap=True,
+                warm_start=True,
+                oob_score=True,
+                max_features="sqrt",
+                random_state=RANDOM_STATE)),
+        ("ExtraTreesClassifier, max_features='log2'",
+            ExtraTreesClassifier(
+                n_estimators=100,
+                bootstrap=True,
+                warm_start=True,
+                max_features='log2',
+                oob_score=True,
+                random_state=RANDOM_STATE)),
+        ("ExtraTreesClassifier, max_features=None",
+            ExtraTreesClassifier(
+                n_estimators=100,
+                bootstrap=True,
+                warm_start=True,
+                max_features=None,
+                oob_score=True,
+                random_state=RANDOM_STATE))]
 
+    # Map a classifier name to a list of (<n_estimators>, <error rate>) pairs.
+    error_rate = OrderedDict((label, []) for label, _ in ensemble_clfs)
+
+    min_estimators = 20
+    max_estimators = 250
+    
+    cancer = load_breast_cancer()
+    for label, clf in ensemble_clfs:
+        for i in range(min_estimators, max_estimators + 1):
+            clf.set_params(n_estimators=i)
+            clf.fit(cancer.data, cancer.target)  # Use cancer data here
+            oob_error = 1 - clf.oob_score_
+            error_rate[label].append((i, oob_error))
+
+    # Generate the "OOB error rate" vs. "n_estimators" plot.
+    for label, clf_err in error_rate.items():
+        xs, ys = zip(*clf_err)
+        plt.plot(xs, ys, label=label)
+
+    plt.xlim(min_estimators, max_estimators)
+    plt.xlabel("n_estimators")
+    plt.ylabel("OOB error rate")
+    plt.legend(loc="upper right")
+    plt.show()
 
 # main function
 if __name__ == '__main__':
@@ -155,9 +222,26 @@ if __name__ == '__main__':
     X = cancer.data  # all feature vectors
     t = cancer.target  # all corresponding labels
     
-    print("Number of 1's in t: ", np.sum(t)) """
+    #print feature names
+    print("Feature names:")
+    print(cancer.feature_names)
+    # get feature names corresponding to indices
+    # 27, 22, 7, 20 and 23
+    print([cancer.feature_names[i] for i in [18, 17, 15, 9, 14]]) """
     
-    classifier_type = sklearn.tree.DecisionTreeClassifier()
+    
+    """ classifier_type = DecisionTreeClassifier()
+    cc = CancerClassifier(classifier = classifier_type)
+    conf_mat = cc.confusion_matrix()
+    print(f"Confusion matrix:      [{conf_mat[0,:]}")
+    print(f"                       {conf_mat[1,:]}]")
+    print("                  Accuracy:", np.round(cc.accuracy(),4))
+    print("                 Precision:", np.round(cc.precision(),4))
+    print("                    Recall:", np.round(cc.recall(),4))
+    print("Cross validation accuracy: ", np.round(cc.cross_validation_accuracy(),4)) """
+    
+    """ # classifier_type random forest
+    classifier_type = RandomForestClassifier(n_estimators=100, max_features=5)
     cc = CancerClassifier(classifier = classifier_type)
     conf_mat = cc.confusion_matrix()
     print(f"Confusion matrix:      [{conf_mat[0,:]}")
@@ -166,3 +250,19 @@ if __name__ == '__main__':
     print("                 Precision:", np.round(cc.precision(),4))
     print("                    Recall:", np.round(cc.recall(),4))
     print("Cross validation accuracy: ", np.round(cc.cross_validation_accuracy(),4))
+    cc.feature_importance() """
+    
+    #_plot_oob_error()
+    
+    """ classifier_type = ExtraTreesClassifier()
+    cc = CancerClassifier(classifier = classifier_type)
+    conf_mat = cc.confusion_matrix()
+    print(f"Confusion matrix:      [{conf_mat[0,:]}")
+    print(f"                       {conf_mat[1,:]}]")
+    print("                  Accuracy:", np.round(cc.accuracy(),4))
+    print("                 Precision:", np.round(cc.precision(),4))
+    print("                    Recall:", np.round(cc.recall(),4))
+    print("Cross validation accuracy: ", np.round(cc.cross_validation_accuracy(),4))
+    print(cc.feature_importance()) """
+    
+    _plot_extreme_oob_error()
